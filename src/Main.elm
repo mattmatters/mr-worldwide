27a31ffe9ba394cd7fb2c1e57c35e76f72179818
Matt Lewis <domattthings@gmail.com>
2018-01-21 21:14:29 -0500

    Blast with a code beatifier

--- src/Main.elm
+++ src/Main.elm
@@ -48,88 +48,103 @@ view model =
 hero : String -> Html Msg
 hero status =
     div
-    [ parrallaxBackground "/pitbull-beer.jpg"]
-    [ h1 [ style [ ("margin", "0") ] ] [ text "Mr. Worldwide Web" ]
-    , h3 [] [ text status ]
-    , p [] [ text "Congratulations, you successfully located the true World Wide Web" ]
-    ]
+        [ parrallaxBackground "/pitbull-beer.jpg" ]
+        [ h1 [ style [ ( "margin", "0" ) ] ] [ text "Mr. Worldwide Web" ]
+        , h3 [] [ text status ]
+        , p [] [ text "Congratulations, you successfully located the true World Wide Web" ]
+        ]
+
 
 description : Html Msg
 description =
     div
-    [ style [ ("display", "flex")
-            , ("flex-direction", "row")
-            , ("flex-wrap", "wrap")
-            , ("justify-content", "center")
-            , ("align-content", "center")
+        [ style
+            [ ( "display", "flex" )
+            , ( "flex-direction", "row" )
+            , ( "flex-wrap", "wrap" )
+            , ( "justify-content", "center" )
+            , ( "align-content", "center" )
             ]
-    ]
-    [ img [ imageStyle
-          , src "/pitbull-zoom.gif"
-          ] []
-    , div [ style [ ("align-self", "center") ]] [ h3 [] [ text "Good job Everyone!" ]
-                                                , p [] [ text "You all get a free car."]
-                                                ]
-    ]
+        ]
+        [ img
+            [ imageStyle
+            , src "/pitbull-zoom.gif"
+            ]
+            []
+        , div [ style [ ( "align-self", "center" ) ] ]
+            [ h3 [] [ text "Good job Everyone!" ]
+            , p [] [ text "You all get a free car." ]
+            ]
+        ]
+
 
 tweetAt : Html Msg
 tweetAt =
     div
-    [ style [ ("display", "flex")
-            , ("flex-direction", "row")
+        [ style
+            [ ( "display", "flex" )
+            , ( "flex-direction", "row" )
+
             -- , ("flex-wrap", "wrap")
-            , ("justify-content", "center")
-            , ("align-content", "center")
-            , ("background-color", "#7DBC9F")
+            , ( "justify-content", "center" )
+            , ( "align-content", "center" )
+            , ( "background-color", "#7DBC9F" )
             ]
-    ]
-    [ tweet
-    , img [ imageStyle
-          , style [ ("max-height", "400px")]
-          , src "/dog.png"
-          ]
-          []
-    ]
+        ]
+        [ tweet
+        , img
+            [ imageStyle
+            , style [ ( "max-height", "400px" ) ]
+            , src "/dog.png"
+            ]
+            []
+        ]
+
 
 imageStyle : Attribute Msg
 imageStyle =
     style
-    [ ("border-radius", "2em")
-    , ("margin", "1em")
-    ]
+        [ ( "border-radius", "2em" )
+        , ( "margin", "1em" )
+        ]
+
 
 parrallaxBackground : String -> Attribute Msg
 parrallaxBackground imgPath =
     style
-    [ ("display", "flex")
-    , ("flex-direction", "column")
-    , ("flex-wrap", "wrap")
-    , ("justify-content", "center")
-    , ("align-content", "flex-end")
-    , ("margin", "0em")
-    , ("width", "100%")
-    , ("height", "500px")
-    , ("background-size", "cover")
-    , ("background-image", "url(" ++ imgPath ++ ")")
-    , ("background-position", "center")
-    , ("background-attachment", "fixed")
-    , ("background-repeat", "no-repeat")
-    ]
+        [ ( "display", "flex" )
+        , ( "flex-direction", "column" )
+        , ( "flex-wrap", "wrap" )
+        , ( "justify-content", "center" )
+        , ( "align-content", "flex-end" )
+        , ( "margin", "0em" )
+        , ( "width", "100%" )
+        , ( "height", "500px" )
+        , ( "background-size", "cover" )
+        , ( "background-image", "url(" ++ imgPath ++ ")" )
+        , ( "background-position", "center" )
+        , ( "background-attachment", "fixed" )
+        , ( "background-repeat", "no-repeat" )
+        ]
+
 
 tweet : Html Msg
 tweet =
     a
-    [ class "twitter-share-button"
-    , href "https://twitter.com/intent/tweet?text=I%20want%20want%20my%20free%20car%20%40pitbull"
-    , style [ ("color", "white")
-            , ("align-self", "center")
-            , ("background-color", "#1DA1F2")
-            , ("padding", "1em")
-            , ("border-radius", ".5em")
-            , ("max-height", "3em")
+        [ class "twitter-share-button"
+        , href "https://twitter.com/intent/tweet?text=I%20want%20want%20my%20free%20car%20%40pitbull"
+        , style
+            [ ( "color", "white" )
+            , ( "align-self", "center" )
+            , ( "background-color", "#1DA1F2" )
+            , ( "padding", "1em" )
+            , ( "border-radius", ".5em" )
+            , ( "max-height", "3em" )
             ]
-    ]
-    [ text "Tweet" ]
+        ]
+        [ text "Tweet" ]
+
+
 
 ---- PROGRAM ----
 
